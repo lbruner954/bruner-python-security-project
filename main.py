212ad760f458f9060c939ce9ed9664a034494a27
lbruner954 <lukebruner47@gmail.com>
2026-09-06 03:00:18 -0400

    Correct syntax errors in main.py
    Fix syntax errors in the requests.get call and print statements.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 
-response = requests.get(https://www.example.com”)
+response = requests.get(https://www.example.com)
 if response.status_code == 200:
 	print(‘Successfully reached example.com”)
 else:
